71bcba9532c2dc9d5ffb67d778c01f3a6e29a075
Gabor Csikos <csiki123@gmail.com>
2016-03-28 14:13:19 +0200

    first commit

---
+++ Design_Patterns/src/com/weebly/gaborcsikos/design_patterns/Man.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.weebly.gaborcsikos.design_patterns;
+
+/**
+ * @author Gabor Csikos
+ *
+ */
+public class Man {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
